b9b8176896af1d3cf307bfad643aa792488aac54
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-11 15:58:23 -0600

    creates hiking project service poro

--- spec/services/hiking_project_service_spec.rb
+++ spec/services/hiking_project_service_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe HikingProjectService do
   it 'returns a default list of trails' do
-    VCR.use_cassettes('services/default_trail_info') do
-      default_trails = HikingProjectService.search(ENV['hiking_project_api_key'])
-      trail = default_trails[:items].first
+    # VCR.use_cassettes('services/default_trail_info') do
+      default_trails = HikingProjectService.search
+      trail = default_trails[:trails].first
 
-      expect(default_trails.count).to eq(50)
-      expect(default_trails).to have_key(:items)
+      expect(default_trails.count).to eq(2)
+      expect(default_trails).to have_key(:trails)
       expect(default_trails).to have_key(:success)
       expect(trail).to have_key(:id)
       expect(trail[:id]).to be_an Integer
@@ -33,7 +33,7 @@ describe HikingProjectService do
       expect(trail[:longitude]).to be_an Float
       expect(trail).to have_key(:latitude)
       expect(trail[:longitude]).to be_an Float 
-    end
+    # end
   end
 end
 
