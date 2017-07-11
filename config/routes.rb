46a2b3d4df1760dff6e6948c89d05edfb7dc3678
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-07-11 16:26:20 -0600

    implement creating a user account

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'landing#index'
+  get '/signup', to: 'users#new'
+  get '/dashboard', to: 'dashboards#show'
+  resources :users, only: [:create]
 end
--- spec/features/guests/guest_can_register_spec.rb
+++ spec/features/guests/guest_can_register_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "Guest registers a new account" do
   scenario "guest can navigate to sign-up page and fill in the form" do
     visit root_path
-    expect(page).to have_content("SIGNUP")
-    click_on("SIGNUP")
+    expect(page).to have_content("SignUp")
+    click_on("SignUp")
     expect(page).to have_current_path("/signup")
     expect(page).to have_content("Username")
     fill_in("Username", with: "Example User Name")
@@ -12,21 +12,23 @@ RSpec.describe "Guest registers a new account" do
     fill_in("Email", with: "example@gmail.com")
     expect(page).to have_content("Password")
     fill_in("Password", with: "password")
-    expect(page).to have_button("Signup")
+    expect(page).to have_button("Submit")
+    click_on("Submit")
     expect(page).to have_current_path("/dashboard")
   end
 
   scenario "guest tries to create an account without a password" do
     visit root_path
-    expect(page).to have_content("SIGNUP")
-    click_on("SIGNUP")
+    expect(page).to have_content("SignUp")
+    click_on("SignUp")
     expect(page).to have_current_path("/signup")
     expect(page).to have_content("Username")
     fill_in("Username", with: "Example User Name")
     expect(page).to have_content("Email")
     fill_in("Email", with: "example@gmail.com")
     expect(page).to have_content("Password")
-    expect(page).to have_button("Signup")
+    expect(page).to have_button("Submit")
+    click_on("Submit")
     expect(page).to have_content("error")
     expect(page).to have_current_path("/signup")
   end
