7740dd312826e32a2dea0262e646952b7407069b
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-11 17:12:20 -0600

    adds hptrail poro

---
+++ app/models/hp_trail.rb
@@ -0,0 +1,18 @@
+class HPTrail
+  attr_reader :id, :name, :summary, :difficulty, :location, :length, :long, :lat, :hp_image, :hp_rating 
+  
+  def initialize(params={})
+    @id = params[:id]
+    @name = params[:name]
+    @summary = params[:summary]
+    @difficulty = params[:difficulty]
+    @location = params[:location]
+    @length = params[:length]
+    @long = params[:longitude]
+    @lat = params[:latitude]
+    @hp_type = params[:type]
+    @hp_url = params[:url]
+    @hp_rating = params[:stars]
+    @hp_image = params[:imgSmall]
+  end
+end
