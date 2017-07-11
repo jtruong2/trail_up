04d77370b4e1594477ad899d9e64151c50f24627
Aaron Olsen <mraaronolsen@gmail.com>
2017-07-11 14:56:51 -0600

    Testing hard push block

---
+++ config/permissions.rb
@@ -0,0 +1,15 @@
+Rails.application.permissions
+
+module Rails
+  class Application
+    def permissions
+      binding.pry
+    end
+  end
+end
+
+class Permissions
+  def self.draw
+    binding.pry
+  end
+end
