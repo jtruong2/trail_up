8755a8a1cabf9bda562b38cdf9da30ca06cad7f0
frustratedswede <andrew.mossefin@gmail.com>
2017-07-12 15:08:37 -0600

    add 'avatar' method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
-  has_many :pictures, as: :imageable
+  has_one :picture, as: :imageable
+
+  def avatar
+    if self.picture == nil
+      image_tag('default_avatar.png')
+    else
+      image_tag("#{self.picture}")
+    end
+  end
+
 end
