e8a9e43474fa00c8b4ad252ffeb15f0b08cda30c
Carl Richmond <richmond.carl@outlook.com>
2017-07-11 19:10:27 -0600

    nav bar now dynamically loads and user can logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   get '/dashboard', to: 'dashboards#show'
 
   resources :users, only: [:create]
