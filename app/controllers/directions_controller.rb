97109066b6c39ac79b2f07e8d20bed8d93c6ba19
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-13 16:18:54 -0600

    removes unused directions code

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -1,9 +1,4 @@
 class DirectionsController < ApplicationController
   def index
-    @coords = Coordinates.new(direction_params)
-  end
-
-  def direction_params
-    params.permit(:orig_lat, :orig_lng, :dest_lat, :dest_lng)
   end
 end
--- app/models/coordinates.rb
+++
@@ -1,9 +0,0 @@
-class Coordinates
-  attr_reader :orig_lat, :orig_lng, :dest_lat, :dest_lng
-  def initialize(coords)
-    @orig_lat   = coords[:orig_lat]
-    @orig_lng   = coords[:orig_lng]
-    @dest_lat   = coords[:dest_lat]
-    @dest_lng   = coords[:dest_lng]
-  end
-end
