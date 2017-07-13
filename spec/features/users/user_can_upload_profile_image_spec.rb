d94af0b0efa8e55e9a2e9aafa60f16b45b9412b4
frustratedswede <andrew.mossefin@gmail.com>
2017-07-13 13:46:05 -0600

    pass rspec

--- spec/features/users/user_can_upload_profile_image_spec.rb
+++ spec/features/users/user_can_upload_profile_image_spec.rb
@@ -4,10 +4,10 @@ describe "user can upload profile image" do
   scenario "user changes image from profile settings" do
     id = create(:user).id
     visit "/users/#{id}"
-    expect(page).to have_link("Edit Settings")
-    click("Edit Settings")
-    expect(path).to eq(edit_user_path)
-    expect(page).to have_link("Create Picture")
+    expect(page).to have_button("Edit Settings")
+    click_on("Edit Settings")
+    expect(current_path).to eq("/users/#{id}/edit")
+    expect(page).to have_button("Create Picture")
 
   end
 end
