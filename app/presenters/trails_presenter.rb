1f6391400fb850adf1ad7031a83bdda98ff0cb3a
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-13 13:37:56 -0600

    fixes coordinate relocation issue

--- app/presenters/trails_presenter.rb
+++ app/presenters/trails_presenter.rb
@@ -1,6 +1,6 @@
 class TrailsPresenter
   def initialize(coords={ :lat=> 39.742043, :lng=> -104.991531 })
-    @results ||= HikingProjectService.search(coords={ :lat=> 39.742043, :lng=> -104.991531 })
+    @results ||= HikingProjectService.search(coords)
   end
 
   def collection
