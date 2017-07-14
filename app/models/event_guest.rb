2659b36a1efe08dbf44304c20da7e4a0e812c586
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-14 17:52:36 -0600

    cleans up unused code

--- app/models/event_guest.rb
+++ app/models/event_guest.rb
@@ -1,5 +1,4 @@
 class EventGuest < ApplicationRecord
   belongs_to :event
-  # belongs_to :guest, foreign_key: 'guest_id'
   belongs_to :guest, :class_name => 'User', :foreign_key => 'guest_id'
 end
