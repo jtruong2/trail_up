1d267e8ffefc67054205802bf5e74ef2de5f71cc
Carl Richmond <richmond.carl@outlook.com>
2017-07-16 10:35:51 -0600

    test coverage for user visiting dashboard complete

--- spec/features/users/user_can_filter_events_search_spec.rb
+++ spec/features/users/user_can_filter_events_search_spec.rb
@@ -2,27 +2,38 @@ require 'rails_helper'
 
 describe "Filtering events" do
   before(:each) do
-    @user_host = create(:user, username: "Host", email: "host@gmail.com")
-    @user_guest = create(:user, username: "Guest", email: "guest@gmail.com")
+    @user = create(:user)
 
     @event_past_hosting_1 = create(:event, archived: true)
     @event_past_hosting_2 = create(:event, archived: true)
-    @event_host_1 = EventHost.create(event_id: event_past_hosting_1.id, host_id: user_host.id)
-    @event_host_2 = EventHost.create(event_id: event_past_hosting_2.id, host_id: user_host.id)
+    @event_host_1 = EventHost.create(event_id: @event_past_hosting_1.id, host_id: @user.id)
+    @event_host_2 = EventHost.create(event_id: @event_past_hosting_2.id, host_id: @user.id)
 
     @event_past_attending_1 = create(:event, archived: true)
     @event_past_attending_2 = create(:event, archived: true)
-    @event_host_1 = EventGuest.create(event_id: event_past_attending_1.id, guest_id: user_guest.id)
-    @event_host_2 = EventGuest.create(event_id: event_past_attending_2.id, guest_id: user_guest.id)
+    @event_host_1 = EventGuest.create(event_id: @event_past_attending_1.id, guest_id: @user.id)
+    @event_host_2 = EventGuest.create(event_id: @event_past_attending_2.id, guest_id: @user.id)
 
     @event_future_hosting_1 = create(:event, archived: false)
     @event_future_hosting_2 = create(:event, archived: false)
-    @event_host_1 = EventHost.create(event_id: event_future_hosting_1.id, host_id: user_host.id)
-    @event_host_2 = EventHost.create(event_id: event_future_hosting_2.id, host_id: user_host.id)
+    @event_host_1 = EventHost.create(event_id: @event_future_hosting_1.id, host_id: @user.id)
+    @event_host_2 = EventHost.create(event_id: @event_future_hosting_2.id, host_id: @user.id)
 
     @event_future_attending_1 = create(:event, archived: false)
     @event_future_attending_2 = create(:event, archived: false)
-    @event_host_1 = EventGuest.create(event_id: event_future_attending_1.id, guest_id: user_guest.id)
-    @event_host_2 = EventGuest.create(event_id: event_future_attending_2.id, guest_id: user_guest.id)
+    @event_host_1 = EventGuest.create(event_id: @event_future_attending_1.id, guest_id: @user.id)
+    @event_host_2 = EventGuest.create(event_id: @event_future_attending_2.id, guest_id: @user.id)
   end
+
+  scenario "user can visit dashboard" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+    visit root_path
+
+    expect(page).to have_content("Dashboard")
+    click_on("Dashboard")
+
+    expect(current_path).to eq('/dashboard')
+  end
+
 end
