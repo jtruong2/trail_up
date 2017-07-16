572e72640806f995e6ed4bb9ec790a9884635702
Carl Richmond <richmond.carl@outlook.com>
2017-07-16 16:46:18 -0600

    js can now hide div elements that do not meet our default criteria

--- app/assets/javascripts/dashboard/dashboard_filter.js
+++ app/assets/javascripts/dashboard/dashboard_filter.js
@@ -3,8 +3,19 @@ var filter_category = "upcoming";
 
 var filter_events = function() {
 
-  var events = document.getElementById('events');
+  var events = document.getElementsByClassName('event')
 
+  var hide_filtered_elements = function(element) {
+    if (!element.classList.contains(filter_category)) {
+      element.style.display='none';
+    }
+  }
+
+  Array.prototype.forEach.call(events, function(element, index) {
+    hide_filtered_elements(element)
+  });
 
 
 }
+
+// document.querySelectorall('events')
