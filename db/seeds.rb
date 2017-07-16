b8bb4e224861904a856a84208ae0ad4b31769f83
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-16 12:33:24 -0600

    adds basic seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,69 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  def self.start
+    seed = Seed.new
+    seed.generate_users
+    seed.generate_trails
+    seed.generate_events
+  end
+
+  def generate_users
+    20.times do |i|
+      user = User.create!(
+        username: Faker::Name.name, 
+        email: Faker::Internet.email,
+        password: 'password'
+      )
+      puts "User #{i}: #{user.username} created!"
+    end
+  end
+
+  def generate_trails
+    10.times do |i|
+      trail = Trail.create!(
+        name: "#{Faker::Hobbit.location}, #{Faker::Address.community}",
+        description: Faker::Hobbit.quote,
+        difficulty: %w(white, blue, green, black diamond, double black diamond).sample,
+        distance: %w(5,10,15,20,25,50,100).sample,
+        rating: %w(1,2,3,4).sample,
+        longitude: longitude,
+        latitude: latitude
+      )
+      puts "User #{i}: #{trail.name} created!"
+    end
+  end
+  
+  def generate_events
+    10.times do |i|
+      trail = Trail.offset(i).first
+      user = User.offset(i).first
+      event = Event.create(
+        trail_id: trail.id,
+        name: Faker::Space.agency,
+        description: Faker::Hobbit.quote,
+        date: Faker::Date.between(Date.today, 1.year.from_now)
+      )
+      EventRole.create(event_id: event.id, user_id: user.id, role: 1)
+      assign_guests(event)
+      puts "Event #{i}: #{event.name} created!"
+    end
+  end
+
+  def assign_guests(event)
+    5.times do |i|
+      users = User.offset(i+5).to_a
+      EventRole.create(event_id: event.id, user_id: users.pop.id)
+      puts "Assigned #{i} users to #{event.name}"
+    end
+  end
+
+
+  def latitude
+    (19.50..44.85).step(0.01).to_a.sample
+  end
+
+  def longitude
+    (-141.75..-88.01).step(0.01).to_a.sample
+  end
+end
+
+Seed.start
