f872cfb5d7b4613e22080bcb940d9b0281c74a13
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-17 08:33:18 -0600

    adds date test for event search

--- spec/features/guests/guest_can_search_events_from_event_search_page_spec.rb
+++ spec/features/guests/guest_can_search_events_from_event_search_page_spec.rb
@@ -45,6 +45,22 @@ RSpec.describe 'As a guest' do
       expect(page).to have_content(event_one.date.strftime("%A %B %e %Y at %l:%M"))
       expect(page).to have_content(event_one.trail.name)
     end
+
+    it 'by date' do
+      create(:trail, name: "Clear Creek Canyon", latitude: 39.7967637, longitude:-105.0426542)
+      event_one = create(:event, trail_id: Trail.first.id)
+      visit new_events_search_path
+      select "Date", from: :search_by
+      fill_in :event_search, with: event_one.date
+      click_on "Search Events"
+      expect(current_path).to eq(events_search_index_path)
+      expect(page).to have_content("Your search returned 1 results")
+      expect(page).to have_content(event_one.name)
+      expect(page).to have_content(event_one.description)
+      expect(page).to have_content(event_one.date.strftime("%A %B %e %Y at %l:%M"))
+      expect(page).to have_content(event_one.trail.name)
+    end
+
     it 'returns no results with no query' do
       create(:trail, name: "Clear Creek Canyon", latitude: 39.7967637, longitude:-105.0426542)
       event_one = create(:event, trail_id: Trail.first.id)
