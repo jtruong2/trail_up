1264972071b3e6828872676558cbd83b98dae3c0
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-18 11:14:23 -0600

    adds admin users before action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,22 +1,24 @@
 class Admin::UsersController < Admin::BaseController
-
+  before_action :set_user, only: [:update, :destroy]
   def index
     @users = User.all
   end
 
   def update
-    @user = User.find_by(slug: params[:id])
-    if current_user.admin?
-      @user.update_attribute(:role, "admin")
-      @user.save
-    end
+    @user.update_attribute(:role, "admin") if current_user.admin?
     redirect_to admin_users_path
   end
 
   def destroy
-    @user = User.find_by(slug: params[:id])
     User.destroy(@user.id) if current_user.admin?
     redirect_to admin_users_path
   end
+
+  private
+
+  def set_user
+    @user = User.find_by(slug: params[:id])
+  end
+
   
 end
