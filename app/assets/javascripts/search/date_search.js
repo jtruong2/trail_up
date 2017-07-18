4a5126fe7d3e1a34932f7f90dde6158044d2c507
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-17 18:07:05 -0600

    add dynamic start and end label tags

--- app/assets/javascripts/search/date_search.js
+++ app/assets/javascripts/search/date_search.js
@@ -5,7 +5,13 @@ var dateSelect = function() {
     const submit = document.getElementById('submit')
     const form = document.querySelector('form')
     const endDate = document.createElement('input')
+    const endDateLabel = document.createElement('label')
+    endDateLabel.innerText = "End Date"
+    const tag = document.querySelector('.location')
 
+    setAttributes(endDateLabel, {
+        "id": "end_date_label",
+    })
     setAttributes(endDate, {
         "type": "date",
         "name": "end_date",
@@ -16,18 +22,27 @@ var dateSelect = function() {
 
     function changeInputFields() {
         if (select.value === 'by_date') {
+            tag.innerText = "Start Date"
             inputField.setAttribute('type', 'date');
             form.insertBefore(endDate, submit)
+            form.insertBefore(endDateLabel, endDate)
         } else if (select.value === 'by_name') {
             setAttributes(inputField, { "type": "text", "placeholder": "event name..." });
+            tag.innerText = "Event Name"
             inputField.setAttribute('type', 'text');
             removeField(form, 'end-date')
+            removeField(form, 'end_date_label')
         } else if (select.value === 'by_trail') {
             setAttributes(inputField, { "type": "text", "placeholder": "trail name..." });
+            tag.innerText = "Trail Name"
             removeField(form, 'end-date')
+            removeField(form, 'end_date_label')
         } else if (select.value === 'by_location') {
             setAttributes(inputField, { "type": "text", "placeholder": "location..." });
+            tag.innerText = "Location"
             removeField(form, 'end-date')
+            removeField(form, 'end_date_label')
+
         }
     }
 }
