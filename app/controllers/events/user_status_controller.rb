3d0480747ffa7e30b20433540df7ab8d564fe977
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-07-18 15:47:26 -0600

    further cleans up permissions

--- app/controllers/events/user_status_controller.rb
+++ app/controllers/events/user_status_controller.rb
@@ -2,7 +2,9 @@ class Events::UserStatusController < ApplicationController
   def update
     event = Event.find(params[:event_id])
     event.user_status_change(status_params)
+    flash[:success] = ["Event Status Updated"]
     redirect_to request.referrer
+    
   end
 
   def status_params
