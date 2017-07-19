2a771fea8e03af697f202c1c8c7bfb2c58ccd3ac
Aaron Olsen <mraaronolsen@gmail.com>
2017-07-19 08:27:25 -0600

    Pausing archive_log for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :current_guest
-  before_action :authorize!, :check_database_archives
+  before_action :authorize!
 
   def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- app/models/archive_log.rb
+++ app/models/archive_log.rb
@@ -1,3 +1,2 @@
-class ArchiveLog <ApplicationRecord
-
+class ArchiveLog < ApplicationRecord
 end
