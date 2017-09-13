263f87ffa48c9f137c7382116052b75ef4384e04
Michael Centrelli <Centm860@newschool.edu>
2017-09-13 17:43:45 -0600

    Switched to Conditions Endpoint

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,13 +3,11 @@ class Weather
   attr_reader :summary, :icon
   
   def initialize(weather_atts={})
-    @summary = weather_atts[:fcttext_metric]
-    @icon = weather_atts[:icon_url]
+    binding.pry
   end
 
   def self.weather_info(lat,lon)
-    WeatherService.new.weather_info(lat,lon)[:forecast][:txt_forecast][:forecastday].map do |raw|
-      Weather.new(raw)
-    end
+    info = WeatherService.new.weather_info(lat,lon)[:current_observation]
+      Weather.new(info)
   end
 end
