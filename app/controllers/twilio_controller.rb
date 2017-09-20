4cc439f5051eefa12cd7272907d2a119acf85e5c
wratterman <will.ratterman@gmail.com>
2017-09-19 19:45:03 -0600

    Add trudy_response method to submit a users text messageto our Watson API and get response back; Can text our 'company' number and get a Trudy welcome response

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,4 +1,6 @@
 require 'twilio-ruby'
+require 'json'
+require 'excon'
 
 class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
@@ -7,7 +9,7 @@ class TwilioController < ApplicationController
   def respond_and_receive
     personal_number = params["From"]
     user_msg        = params["Body"]
-    response_msg    = "You would like that...Des"
+    response_msg    = trudy_response(user_msg)
     app_number      = ENV["PHONE_NUMBER"]
     twilio_id       = ENV["ACCOUNT_SID"]
     auth_token      = ENV["AUTH_TOKEN"]
@@ -19,5 +21,15 @@ class TwilioController < ApplicationController
       from: app_number,
       body: response_msg
     )
+  end
+
+  def trudy_response(user_msg)
+    response = Excon.post(ENV["TRUDY_URL"],
+      text: user_msg,
+      headers: { "Content-Type" => "application/json" },
+      user: ENV["USERNAME"],
+      password: ENV["PASSWORD"],
+    )
+    JSON.parse(response.body, symbolize_names: true)[:output][:text].first
   end
 end
